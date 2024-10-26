b86b0d4834c4da3c76c37b3cc64f570c5e0ead4e
Cheeter <cheeter@myyahoo.com>
2024-10-26 15:46:59 +0200

    print command added.

---
+++ src/main/java/de/cheeter/mumtad/yakeo/print.java
@@ -0,0 +1,14 @@
+package de.cheeter.mumtad.yakeo;
+
+/**
+ @author Cheeter Development
+ @version 1.0
+ @since 1.0 ALPHA
+ */
+
+public class print {
+
+    public static void print(Object content) {
+    System.out.println(content)
+    }
+}
